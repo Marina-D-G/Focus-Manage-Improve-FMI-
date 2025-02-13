e6fd3caa513e132227c8dc0b6d297b0e36877a15
Marina-D-G <marina_dg@abv.bg>
2025-02-13 20:33:07 +0200

    add home page with quotes

--- base/views.py
+++ base/views.py
@@ -1,17 +1,38 @@
+import requests
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth.models import User
+from django.conf import settings
+from django.utils.timezone import now
 from .forms import ProfileUpdateForm
 from .models import Profile
+from tasks.models import TodoItem
+from calendars.models import Event 
+from translate import Translator
+
+def get_info_homepage(request):
+    notifications_list = list(request.user.notifications.unread())
+    request.user.notifications.mark_all_as_read()
+    quote, author = random_quotes()
+    today = now().date()
+    unfinished_tasks_count = TodoItem.objects.filter(deadline=today, todo_list__users=request.user).count()
+    today_events_count = Event.objects.filter(date=today, calendar__users=request.user).count()
+    context = {
+        "notifications_list": notifications_list,
+        "quote": quote,
+        "author": author,
+        "unfinished_tasks_count": unfinished_tasks_count,
+        "today_events_count": today_events_count
+    }
+    return context
 
 
 def home(request):
     if request.user.is_authenticated:
-        notifications_list = list(request.user.notifications.unread())
-        request.user.notifications.mark_all_as_read()
-        return render(request, "home_logged_in.html", {"notifications_list": notifications_list})
+        context = get_info_homepage(request)
+        return render(request, "home_logged_in.html", context)
     else:
         return render(request, "home_guest.html")
 
@@ -66,3 +87,27 @@ def change_password(request):
         form = PasswordChangeForm(request.user)
 
     return render(request, 'change_password.html', {'form': form})
+
+
+def translate_text(text):
+    translator = Translator(to_lang="bg")
+    translated_text = translator.translate(text)
+    return translated_text
+
+
+def random_quotes():
+    quote_api_url = 'https://zenquotes.io/api/random'
+    response = requests.get(quote_api_url)
+
+    quote_text = "No quote available"
+    author = "Unknown"
+
+    if response.status_code == 200:
+        quote_data = response.json()
+        if isinstance(quote_data, list) and len(quote_data) > 0:
+            quote_text = quote_data[0].get('q', "No quote available")
+            author = quote_data[0].get('a', "Unknown")
+
+    translated_quote = translate_text(quote_text)
+    translated_author = translate_text(author)
+    return translated_quote, translated_author
